48b67deded6e88a4104d2a67771be1db5edc408e
Miika Rannikko <miika.rannikko@gmail.com>
2020-09-18 17:22:30 +0300

    Draft core app

---
+++ app.py
@@ -0,0 +1,89 @@
+from flask import Flask
+from flask import redirect, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
+
+@app.route("/",methods=['GET','POST'])
+def index():
+    return render_template("index.html") 
+
+@app.route("/profile",methods=['GET','POST'])
+def profile():
+    # Get count of all logged expenses (will be changed to only session user)
+    sql = "SELECT COUNT(expense_id) FROM expenses"
+    result = db.session.execute(sql)
+    count = result.fetchone()[0]
+    return render_template("profile.html",expense_count=count) 
+
+@app.route("/login",methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    # Give session the username
+    session["username"] = username
+    # Verify user and password
+    sql = "SELECT password FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone() 
+    if user == None:
+        del session["username"]
+    else:
+        hash_value = user[0]
+        if check_password_hash(hash_value,password):
+            return redirect("/profile")
+        else:
+            del session["username"] 
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html") 
+
+@app.route("/registration-complete", methods=["GET","POST"])
+def registration_complete():
+    username = request.form["username"]
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    email = request.form["email"]
+    password = request.form["password"]
+    hash_value = generate_password_hash(password)
+    sql = "INSERT INTO users (username, first_name, last_name, email, password, registration_time) VALUES (:username,:first_name,:last_name,:email,:password, NOW())"    
+    db.session.execute(sql, {"username":username,"first_name":first_name,"last_name":last_name,"password":hash_value,"email":email})
+    db.session.commit()
+    return redirect("/")
+
+@app.route("/new-expense")
+def new_expense():
+    return render_template("expense.html")
+    
+@app.route("/send-expense", methods=["POST"])
+def send_expense():
+    amount = request.form["amount"]
+    expense_date = request.form["expense_date"]
+    category = request.form["category"]
+    note = request.form["note"]
+    # Fetch used_id
+    username = session["username"]
+    sql = "SELECT user_id FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user_id = result.fetchone()[0]
+    sql = "INSERT INTO expenses (amount,expense_date,category,user_id, note) VALUES (:amount,:expense_date,:category,:user_id,:note)"
+    db.session.execute(sql, {"amount":amount, "expense_date":expense_date, "category":category, "user_id":user_id, "note":note})
+    db.session.commit()
+    return redirect("/profile")
+
+@app.route("/new-income")
+def new_income():
+    return render_template("income.html")
