f3584c54859b210b45194561953100a04b9fd118
Miika Rannikko <miika.rannikko@gmail.com>
2020-09-20 09:46:54 +0300

    SQL schema added

---
+++ schema.sql
@@ -0,0 +1,3 @@
+CREATE TABLE users (user_id SERIAL PRIMARY KEY, username VARCHAR(20), first_name VARCHAR(50), last_name VARCHAR(50), password VARCHAR, registration_time TIMESTAMP, email VARCHAR);
+CREATE TABLE expenses (expense_id SERIAL PRIMARY KEY, expense_date DATE, amount NUMERIC, category VARCHAR(50), user_id INTEGER, note VARCHAR);
+CREATE TABLE incomes (income_id SERIAL PRIMARY KEY, income_date DATE, amount NUMERIC, category VARCHAR(50), user_id INTEGER, note VARCHAR);
