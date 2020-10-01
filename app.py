56fb6ccb53753a89876e8bf737e9d787421908e2
Miika Rannikko <miika.rannikko@gmail.com>
2020-10-01 15:41:53 +0300

    Dashboard

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 from flask import Flask
 from flask import redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
@@ -16,9 +18,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 engine_name = db.get_engine(app=app)
 
-# how to pass username to pandas query
-# Put result to plotly graphs
-
 @app.route("/",methods=['GET','POST'])
 def index():
     return render_template("index.html") 
@@ -32,11 +31,34 @@ def profile():
     expenses_count = result.fetchone()[0]
     # Return table of expenses as dataframe
     df = pd.read_sql_query("SELECT expense_date,amount,category,note FROM expenses JOIN users ON expenses.user_id = users.user_id WHERE users.username=%(u)s ORDER BY expense_date DESC",engine_name, params={"u":username})
-    # Create pie chart of expense structure
-    df_pie = df.groupby('category', as_index=False).agg({"amount": "sum"})
-    expense_pie = px.pie(data_frame=df_pie,values='amount',names='category',color='category')
+    df['expense_date'] = pd.to_datetime(df['expense_date']) 
+    df['year'] = df['expense_date'].dt.year.astype(int)
+    df['month'] = df['expense_date'].dt.month.astype(int)
+    current_year = pd.datetime.now().year
+    # Create monthly expenses vs ly
+    df_two_years = df.where(df['year'] >= (current_year-1))
+    df_two_years['year'] = df_two_years['year'].astype(str)
+    df_bar = df_two_years.groupby(['category','year','month'], as_index=False).agg({"amount": "sum"})
+    expense_bar = px.bar(data_frame=df_bar,x='month',y='amount',color='year',orientation='v',barmode='group',title='Total monthly expenses vs LY',width=500,height=500)
+    expense_bar = pt.offline.plot(expense_bar,output_type='div')
+    # Create pie chart of expense category % for past 12 months
+    start_date = datetime.today().replace(day=1) + relativedelta(months=-12)
+    end_date = datetime.today()
+    mask = (df['expense_date'] > start_date) & (df['expense_date'] <= end_date)
+    df_12m = df.loc[mask]
+    df_pie = df_12m.groupby('category', as_index=False).agg({"amount": "sum"})
+    expense_pie = px.pie(data_frame=df_pie,values='amount',names='category',color='category',title='Total expenses per category (last 12m)',width=500,height=500)
     expense_pie = pt.offline.plot(expense_pie,output_type='div')
-    return render_template("profile.html",expense_count=expenses_count,pie=expense_pie,tables=[df.to_html(classes='data',header="true",justify="center",max_rows=10,index=False)]) 
+    # Create stacked bar chart of expense category % per month
+    df_12m['month'] = df_12m['month'].astype(str)
+    df_12m['year'] = df_12m['year'].astype(str)
+    df_12m['yearmonth'] = df_12m['year'].str.cat(df_12m['month'], sep ="-") 
+    df_bar_two = df_12m.groupby(['category','yearmonth'], as_index=False).agg({"amount": "sum"})
+    df_bar_two['category%'] = df_bar_two['amount'] / df_bar_two.groupby('yearmonth')['amount'].transform('sum')
+    expense_bar_two = px.bar(data_frame=df_bar_two,x='yearmonth',y='category%',color='category',orientation='v',barmode='relative',title='Category shares of expenses per month',width=500,height=500)
+    expense_bar_two = pt.offline.plot(expense_bar_two,output_type='div')
+    # Create income vs expenses
+    return render_template("profile.html",expense_count=expenses_count,pie=expense_pie,bar_one=expense_bar,bar_two=expense_bar_two,tables=[df.to_html(classes='data',header="true",justify="center",max_rows=10,index=False)]) 
 
 @app.route("/login",methods=["POST"])
 def login():
@@ -75,10 +97,20 @@ def registration_complete():
     email = request.form["email"]
     password = request.form["password"]
     hash_value = generate_password_hash(password)
-    sql = "INSERT INTO users (username, first_name, last_name, email, password, registration_time) VALUES (:username,:first_name,:last_name,:email,:password, NOW())"    
-    db.session.execute(sql, {"username":username,"first_name":first_name,"last_name":last_name,"password":hash_value,"email":email})
-    db.session.commit()
-    return redirect("/")
+    sql = "SELECT username FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    username_db = result.fetchone() 
+    if username_db:
+        return redirect("/duplicate-username")
+    else:
+        sql = "INSERT INTO users (username, first_name, last_name, email, password, registration_time) VALUES (:username,:first_name,:last_name,:email,:password, NOW())"    
+        db.session.execute(sql, {"username":username,"first_name":first_name,"last_name":last_name,"password":hash_value,"email":email})
+        db.session.commit()
+        return redirect("/")
+
+@app.route("/duplicate-username")
+def duplicate_username():
+    return render_template("duplicate_username.html") 
 
 @app.route("/new-expense")
 def new_expense():
@@ -90,7 +122,6 @@ def send_expense():
     expense_date = request.form["expense_date"]
     category = request.form["category"]
     note = request.form["note"]
-    # Fetch used_id
     username = session["username"]
     sql = "SELECT user_id FROM users WHERE username=:username"
     result = db.session.execute(sql, {"username":username})
@@ -110,7 +141,6 @@ def send_income():
     income_date = request.form["income_date"]
     category = request.form["category"]
     note = request.form["note"]
-    # Fetch used_id
     username = session["username"]
     sql = "SELECT user_id FROM users WHERE username=:username"
     result = db.session.execute(sql, {"username":username})
