f818a4b7e4b164ad703a87f73cc864b57a10c83a
Miika Rannikko <miika.rannikko@gmail.com>
2020-10-12 10:54:21 +0300

    Uniform list spacing

--- charts.py
+++ charts.py
@@ -10,27 +10,27 @@ from app import app
 engine_name = db.get_engine(app=app)
 
 def recorded_counts(username):
-    df_expenses = pd.read_sql_query("SELECT expense_id FROM expenses JOIN users ON expenses.user_id = users.user_id WHERE users.username=%(u)s",engine_name, params={"u":username})
+    df_expenses = pd.read_sql_query("SELECT expense_id FROM expenses JOIN users ON expenses.user_id = users.user_id WHERE users.username=%(u)s",engine_name,params={"u":username})
     expenses_count = len(df_expenses.index)
-    df_incomes = pd.read_sql_query("SELECT income_id FROM incomes JOIN users ON incomes.user_id = users.user_id WHERE users.username=%(u)s",engine_name, params={"u":username})
+    df_incomes = pd.read_sql_query("SELECT income_id FROM incomes JOIN users ON incomes.user_id = users.user_id WHERE users.username=%(u)s",engine_name,params={"u":username})
     incomes_count = len(df_incomes.index)
     return (expenses_count,incomes_count)
 
 def search_expenses(username,start_date,end_date,category="",note_text=""):
-    df = pd.read_sql_query("SELECT expense_date,amount,category,note FROM expenses JOIN users ON expenses.user_id = users.user_id WHERE users.username=%(u)s AND expense_date BETWEEN %(start)s AND %(end)s",engine_name, params={"u":username,"start":start_date,"end":end_date})
+    df = pd.read_sql_query("SELECT expense_date,amount,category,note FROM expenses JOIN users ON expenses.user_id = users.user_id WHERE users.username=%(u)s AND expense_date BETWEEN %(start)s AND %(end)s",engine_name,params={"u":username,"start":start_date,"end":end_date})
     if len(category)>0:
         df = df[df['category']==category]
     if len(note_text)>0:
-        df = df[df['note'].str.contains(note_text, na=False)]
+        df = df[df['note'].str.contains(note_text,na=False)]
     total = df["amount"].sum(skipna=True)
     return df,total
 
 def search_incomes(username,start_date,end_date,category="",note_text=""):
-    df = pd.read_sql_query("SELECT income_date,amount,category,note FROM incomes JOIN users ON incomes.user_id = users.user_id WHERE users.username=%(u)s AND income_date BETWEEN %(start)s AND %(end)s",engine_name, params={"u":username,"start":start_date,"end":end_date})
+    df = pd.read_sql_query("SELECT income_date,amount,category,note FROM incomes JOIN users ON incomes.user_id = users.user_id WHERE users.username=%(u)s AND income_date BETWEEN %(start)s AND %(end)s",engine_name,params={"u":username,"start":start_date,"end":end_date})
     if len(category)>0:
         df = df[df['category']==category]
     if len(note_text)>0:
-        df = df[df['note'].str.contains(note_text, na=False)]
+        df = df[df['note'].str.contains(note_text,na=False)]
     total = df["amount"].sum(skipna=True)
     return df,total
 
@@ -38,12 +38,12 @@ def search_net_balance(username,start_date,end_date,category="",note_text=""):
     df_incomes,total_incomes = search_incomes(username,start_date,end_date,category,note_text)
     df_expenses,total_expenses = search_expenses(username,start_date,end_date,category,note_text)
     total = total_incomes - total_expenses
-    expenses_grouped = df_expenses.groupby(['category'], as_index=False).agg({"amount": "sum"})
+    expenses_grouped = df_expenses.groupby(['category'], as_index=False).agg({"amount":"sum"})
     expenses_grouped.loc['Total expenses'] = expenses_grouped.sum()
-    expenses_grouped.at['Total expenses', 'category'] = 'TOTAL EXPENSES'
-    incomes_grouped = df_incomes.groupby(['category'], as_index=False).agg({"amount": "sum"})
+    expenses_grouped.at['Total expenses','category'] = 'TOTAL EXPENSES'
+    incomes_grouped = df_incomes.groupby(['category'], as_index=False).agg({"amount":"sum"})
     incomes_grouped.loc['Total incomes'] = incomes_grouped.sum()
-    incomes_grouped.at['Total incomes', 'category'] = 'TOTAL INCOMES'
+    incomes_grouped.at['Total incomes','category'] = 'TOTAL INCOMES'
     df = pd.concat([incomes_grouped,expenses_grouped])
     return df,total
 
@@ -56,7 +56,7 @@ def chart_monthly_expenses(username):
         current_year = pd.datetime.now().year
         df_filtered = df.where(df['year'] >= (current_year-1))
         df_filtered['year'] = df_filtered['year'].astype(str)
-        df_grouped = df_filtered.groupby(['category','year','month'], as_index=False).agg({"amount": "sum"})
+        df_grouped = df_filtered.groupby(['category','year','month'], as_index=False).agg({"amount":"sum"})
         chart = px.bar(data_frame=df_grouped,x='month',y='amount',color='year',orientation='v',barmode='group',title='Total monthly expenses vs LY',width=500,height=500)
         chart = pt.offline.plot(chart,output_type='div')
     else:
@@ -71,7 +71,7 @@ def chart_expense_categories(username):
         end_date = datetime.today().replace(day=1) + relativedelta(months=+1)
         mask = (df['expense_date'] >= start_date) & (df['expense_date'] < end_date)
         df_filtered = df.loc[mask]
-        df_grouped = df_filtered.groupby('category', as_index=False).agg({"amount": "sum"})
+        df_grouped = df_filtered.groupby('category', as_index=False).agg({"amount":"sum"})
         chart = px.pie(data_frame=df_grouped,values='amount',names='category',color='category',title='Category shares of expenses for last 12 months',width=500,height=500)
         chart = pt.offline.plot(chart,output_type='div')
     else:
@@ -88,7 +88,7 @@ def chart_monthly_expense_categories(username):
         df_filtered = df.loc[mask]
         df_filtered['yearmonth'] = pd.to_datetime(df_filtered['expense_date']).dt.to_period('M')
         df_filtered['yearmonth'] = df_filtered['yearmonth'].astype(str)
-        df_grouped = df_filtered.groupby(['category','yearmonth'], as_index=False).agg({"amount": "sum"})
+        df_grouped = df_filtered.groupby(['category','yearmonth'], as_index=False).agg({"amount":"sum"})
         df_grouped['category%'] = df_grouped['amount'] / df_grouped.groupby('yearmonth')['amount'].transform('sum')
         chart = px.bar(data_frame=df_grouped,x='yearmonth',y='category%',color='category',orientation='v',barmode='relative',title='Category shares of expenses per month',width=500,height=500)
         chart = pt.offline.plot(chart,output_type='div')
@@ -102,7 +102,7 @@ def chart_monthly_expenses_vs_incomes(username):
         start_date = datetime.today().replace(day=1) + relativedelta(months=-11)
         end_date = datetime.today().replace(day=1) + relativedelta(months=+1)
         # Income
-        df_incomes = pd.read_sql_query("SELECT income_date,amount,category,note FROM incomes JOIN users ON incomes.user_id = users.user_id WHERE users.username=%(u)s ORDER BY income_date DESC",engine_name, params={"u":username})
+        df_incomes = pd.read_sql_query("SELECT income_date,amount,category,note FROM incomes JOIN users ON incomes.user_id = users.user_id WHERE users.username=%(u)s ORDER BY income_date DESC",engine_name,params={"u":username})
         df_incomes['income_date'] = pd.to_datetime(df_incomes['income_date']) 
         mask = (df_incomes['income_date'] >= start_date) & (df_incomes['income_date'] < end_date)
         df_incomes_filtered = df_incomes.loc[mask]
@@ -111,13 +111,13 @@ def chart_monthly_expenses_vs_incomes(username):
         df_incomes_grouped = df_incomes_filtered.groupby(['yearmonth'], as_index=False).agg({"amount": "sum"})
         df_incomes_grouped = df_incomes_grouped.assign(type='income')
         # Expenses
-        df_expenses = pd.read_sql_query("SELECT expense_date,amount,category,note FROM expenses JOIN users ON expenses.user_id = users.user_id WHERE users.username=%(u)s ORDER BY expense_date DESC",engine_name, params={"u":username})
+        df_expenses = pd.read_sql_query("SELECT expense_date,amount,category,note FROM expenses JOIN users ON expenses.user_id = users.user_id WHERE users.username=%(u)s ORDER BY expense_date DESC",engine_name,params={"u":username})
         df_expenses['expense_date'] = pd.to_datetime(df_expenses['expense_date']) 
         mask = (df_expenses['expense_date'] >= start_date) & (df_expenses['expense_date'] < end_date)
         df_expenses_filtered = df_expenses.loc[mask]
         df_expenses_filtered['yearmonth'] = pd.to_datetime(df_expenses_filtered['expense_date']).dt.to_period('M')
         df_expenses_filtered['yearmonth'] = df_expenses_filtered['yearmonth'].astype(str)
-        df_expenses_grouped = df_expenses_filtered.groupby(['yearmonth'], as_index=False).agg({"amount": "sum"})
+        df_expenses_grouped = df_expenses_filtered.groupby(['yearmonth'], as_index=False).agg({"amount":"sum"})
         df_expenses_grouped = df_expenses_grouped.assign(type='expense')
         # Combined
         df_concat = pd.concat([df_expenses_grouped,df_incomes_grouped])
